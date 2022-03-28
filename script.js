3f9f2f2bf7b99aee0826c6e54b1113cce32e6512
hassanbaryal <h.baryal2@gmail.com>
2022-03-28 18:00:59 -0400

    Added functionality to the rainbow button

--- script.js
+++ script.js
@@ -8,14 +8,40 @@ let loc = 0;
 const container = document.querySelector('.grid-container')
 let gridArray;
 let isDown = false
-
+let rainbowMode = false
+// Array of random colors used for rainbow mode
+let colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+let colorArrayIndex = 0
 
 document.addEventListener('mousedown', function (e) {isDown = true})
 document.addEventListener('mouseup', function (e) {isDown = false})
 document.querySelector(".clear-grid").addEventListener('click', clearGrid)
+const rainbowBtn = document.querySelector('.rainbow-btn')
 const backgroundColor = document.querySelector(".bg-color-selector")
 const squareColor = document.querySelector(".color-selector")
 const gridSize = document.querySelector(".size-slider")
+
+// When rainbow button is clicked, rainbow mode is turned on (true) and the color of each square
+// is reandomly selected from colorArray
+rainbowBtn.addEventListener('click', function (e) {
+    if(rainbowMode) {
+        document.querySelector('.rainbow-mode').textContent = `OFF`
+        rainbowMode = false
+    } else {
+        document.querySelector('.rainbow-mode').textContent = `ON`
+        rainbowMode = true
+        squareColor.setAttribute('value', `${colorArray[colorArrayIndex]}`)
+    }
+})
 
 // This event listenener changes the colour of the background/uncoloured squares as the user
 // changes the background colour on the settings UI (input element)
@@ -26,6 +52,8 @@ backgroundColor.addEventListener('input', function (e) {
         }
     }
 })
+
+
 
 // This event listener changes the size of the grid and user changes the grid size slider
 gridSize.addEventListener('input', function (e) {
@@ -45,7 +73,7 @@ function createGrid (size) {
         
         container.appendChild(sqr)
     }
-    console.log(backgroundColor.value)
+
     const grid = document.querySelectorAll('.square')
 
     grid.forEach(square => {
@@ -59,7 +87,18 @@ function createGrid (size) {
 
 function colourSquare (e) {
     if (isDown) {
-        e.target.style.setProperty('background-color', `${squareColor.value}`)
+        if (rainbowMode) {
+            
+            console.log(colorArrayIndex)
+            e.target.style.setProperty('background-color', `${colorArray[colorArrayIndex]}`)
+            colorArrayIndex++
+            if (colorArrayIndex > (colorArray.length-1)) {
+                colorArrayIndex = 0
+            }
+            squareColor.setAttribute('value', `${colorArray[colorArrayIndex]}`)
+        } else {
+            e.target.style.setProperty('background-color', `${squareColor.value}`)
+        }
         e.target.classList.add("colored")
     }
 }
